33aa7bfc4ad044d1f2836182bf34ae88c2ea74bb
Thanh Hóa Hiếu <chiendahust@gmail.com>
2021-09-24 12:38:55 +0700

    fix train function

--- train.py
+++ train.py
@@ -9,12 +9,13 @@ def train(dataloader,model,criterion,optimizer,device="cpu",isEval=False):
         correct = 0
         
     for data,target in loader:
+        optimizer.zero_grad()
         data,target = data.to(device),target.to(device)
         pred = model(data)
         loss = criterion(pred,target)
         loss.backward()
         optimizer.step()
-        losses.append(loss.item())
+        
         
         if isEval:
             nums += data.shape[0]
@@ -22,7 +23,7 @@ def train(dataloader,model,criterion,optimizer,device="cpu",isEval=False):
             correct += (pred_label==target).sum()
         
         loader.set_postfix(train_loss_batch = loss.item())
-    
+        losses.append(loss.item())
     if isEval:
         return sum(losses)/len(losses),correct/nums*100
 
